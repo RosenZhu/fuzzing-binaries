582880336b134b435da4e0648bed30be0ea501d0
RosenZhu <rosenzhu@hotmail.com>
2018-12-07 21:33:13 +1100

    add

---
+++ AVR-Keyboard_0.c
@@ -0,0 +1,432 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+
+
+int remainder_check(char* parameter_1,int checksum_div,int length);
+
+
+int remainder_check(char* parameter_1,int checksum_div,int length)
+{
+   if(strlen(parameter_1)!=length)  return 0;
+   int sum = 0;
+   for(int i=0;i<length;i++)  sum = sum + parameter_1[i];
+   if((sum % checksum_div==0))  return 1;
+   else return 0;
+}
+
+void release_column( int parameter_1);
+void usb_keyboard_send();
+void send();
+void key_press( char parameter_1,int uni_para);
+void pull_column( int parameter_1);
+void setup_bounce_timer();
+void setup_leds();
+void setup_io_pins();
+unsigned int usb_configured();
+void usb_init();
+void init();
+void release_column( int parameter_1)
+{
+}
+void usb_keyboard_send()
+{
+	double double_1 = 0;
+	double double_2 = 0;
+	unsigned int unsigned_int_1 = 0;
+	unsigned int unsigned_int_2 = 0;
+	unsigned int unsigned_int_3 = 0;
+	int int_1 = 0;
+	int int_2 = 0;
+	unsigned int unsigned_int_4 = 0;
+	unsigned int unsigned_int_5 = 0;
+	int int_3 = 0;
+	long long_1 = 0;
+	long long_2 = 0;
+	long long_3 = 0;
+	double double_3 = 0;
+	float float_1 = 0;
+	float float_2 = 0;
+	double_1 = log ( double_2 ) ;
+	if(1)
+	{
+	}
+	unsigned_int_3 = unsigned_int_1 * unsigned_int_2;
+	int_1 = int_1;
+	int_1 = int_1;
+	double_2 = tan ( double_2 ) ;
+	int looper_1 = 0;
+	while(looper_1 < 8)
+	{
+		double double_1 = 0;
+		double double_2 = 0;
+		unsigned int unsigned_int_1 = 0;
+		unsigned int unsigned_int_2 = 0;
+		unsigned int unsigned_int_3 = 0;
+		int int_1 = 0;
+		int int_2 = 0;
+		unsigned int unsigned_int_4 = 0;
+		unsigned int unsigned_int_5 = 0;
+		int int_3 = 0;
+		long long_1 = 0;
+		long long_2 = 0;
+		long long_3 = 0;
+		double double_3 = 0;
+		float float_1 = 0;
+		float float_2 = 0;
+		looper_1 += 1;
+		char controller_2[2];
+		fgets(controller_2 ,2 ,stdin);
+		if( strcmp( controller_2, "X") > 0)
+		{
+		}
+		float_1 = float_1 + float_2;
+		char controller_3[3];
+		fgets(controller_3 ,3 ,stdin);
+		if( strcmp( controller_3, "S8") < 0)
+		{
+		}
+		if(1)
+		{
+		}
+		double_2 = tan ( double_2 ) ;
+		int_2 = int_1;
+		unsigned_int_1 = unsigned_int_3;
+	}
+	unsigned_int_5 = unsigned_int_4 + unsigned_int_4;
+	unsigned_int_4 = unsigned_int_4;
+	for(int looper_2=0; looper_2<2;looper_2++)
+	{
+		double_2 = sqrt ( double_2 ) ;
+	}
+	int_2 = int_1 * int_3;
+	long_3 = long_1 + long_2;
+	double_3 = double_1;
+}
+void send()
+{
+	char char_1 = 0;
+	double double_1 = 0;
+	double double_2 = 0;
+	long long_1 = 0;
+	long long_2 = 0;
+	char_1 = char_1;
+	for(int looper_1=0; looper_1<2;looper_1++)
+	{
+		double_2 = double_1 + double_2;
+	}
+	usb_keyboard_send();
+
+	long_1 = long_1 * long_2;
+	double_1 = cos ( double_2 ) ;
+}
+void key_press( char parameter_1,int uni_para)
+{
+	double double_1 = 0;
+	double double_2 = 0;
+	double double_3 = 0;
+	int int_1 = 0;
+	unsigned int unsigned_int_1 = 0;
+	unsigned int unsigned_int_2 = 0;
+	unsigned int unsigned_int_3 = 0;
+	char char_1 = 0;
+	char char_2 = 0;
+	char * vul_var;
+	vul_var=(char*)malloc(20*sizeof(char));
+	double_1 = fmod ( double_1 , double_2 ) ;
+	double_1 = fabs ( double_2 ) ;
+	double_2 = ldexp ( double_3 , int_1 ) ;
+	char controller4vul_3[1];
+	fgets(controller4vul_3 ,2 ,stdin);
+	if( strcmp( controller4vul_3, "$") > 0)
+	{
+		strcpy(vul_var, "CWE-761");
+		if(uni_para == 592)
+		{
+			vul_var = vul_var + 1;
+		}
+		free(vul_var);
+		double_3 = acos ( double_2 ) ;
+	}
+	else
+	{
+		double double_1 = 0;
+		double double_2 = 0;
+		double double_3 = 0;
+		int int_1 = 0;
+		unsigned int unsigned_int_1 = 0;
+		unsigned int unsigned_int_2 = 0;
+		unsigned int unsigned_int_3 = 0;
+		char char_1 = 0;
+		char char_2 = 0;
+		for(int looper_1=0; looper_1<10;looper_1++)
+		{
+			double double_1 = 0;
+			double double_2 = 0;
+			double double_3 = 0;
+			int int_1 = 0;
+			unsigned int unsigned_int_1 = 0;
+			unsigned int unsigned_int_2 = 0;
+			unsigned int unsigned_int_3 = 0;
+			char char_1 = 0;
+			char char_2 = 0;
+			char_1 = char_2;
+		}
+		unsigned_int_3 = unsigned_int_1 * unsigned_int_2;
+	}
+	double_1 = floor ( double_1 ) ;
+}
+void pull_column( int parameter_1)
+{
+	double double_1 = 0;
+	double_1 = asin ( double_1 ) ;
+}
+void setup_bounce_timer()
+{
+	double double_1 = 0;
+	double double_2 = 0;
+	double double_3 = 0;
+	double double_4 = 0;
+	double_1 = ceil ( double_1 ) ;
+	double_1 = asin ( double_1 ) ;
+	double_2 = floor ( double_1 ) ;
+	double_3 = exp ( double_4 ) ;
+}
+void setup_leds()
+{
+	float float_1 = 0;
+	float float_2 = 0;
+	unsigned int unsigned_int_1 = 0;
+	unsigned int unsigned_int_2 = 0;
+	float_1 = float_1 * float_2;
+	unsigned_int_1 = unsigned_int_2;
+}
+void setup_io_pins()
+{
+	double double_1 = 0;
+	double double_2 = 0;
+	float float_1 = 0;
+	float float_2 = 0;
+	short short_1 = 0;
+	short short_2 = 0;
+	double double_3 = 0;
+	double_1 = pow ( double_2 , double_2 ) ;
+	float_2 = float_1 * float_1;
+	short_1 = short_2;
+	for(int looper_1=0; looper_1<3;looper_1++)
+	{
+		double double_1 = 0;
+		double double_2 = 0;
+		float float_1 = 0;
+		float float_2 = 0;
+		short short_1 = 0;
+		short short_2 = 0;
+		double double_3 = 0;
+		double_3 = cosh ( double_1 ) ;
+		double_3 = sinh ( double_3 ) ;
+	}
+}
+unsigned int usb_configured()
+{
+	unsigned int unsigned_int_1 = 0;
+	return unsigned_int_1;
+}
+void usb_init()
+{
+	double double_1 = 0;
+	int int_1 = 0;
+	double double_2 = 0;
+	int int_2 = 0;
+	int int_3 = 0;
+	double double_3 = 0;
+	int int_4 = 0;
+	double_1 = floor ( double_1 ) ;
+	int_1 = int_1;
+	double_1 = double_2;
+	int looper_1 = 0;
+	while(looper_1 < 5)
+	{
+		looper_1 += 1;
+		double_1 = pow ( double_1 , double_1 ) ;
+	}
+	double_2 = acos ( double_2 ) ;
+	int_3 = int_2 + int_3;
+	double_2 = tan ( double_2 ) ;
+	double_3 = fmod ( double_3 , double_1 ) ;
+	int_1 = int_4;
+}
+void init()
+{
+	double double_1 = 0;
+	double double_2 = 0;
+	int int_1 = 0;
+	int int_2 = 0;
+	double double_3 = 0;
+	double double_4 = 0;
+	int int_3 = 0;
+	double double_5 = 0;
+	double double_6 = 0;
+	double double_7 = 0;
+	double double_8 = 0;
+	unsigned int unsigned_int_1 = 0;
+	double double_9 = 0;
+	double double_10 = 0;
+	unsigned int unsigned_int_2 = 0;
+	setup_leds();
+
+	double_1 = log10 ( double_2 ) ;
+	int_1 = int_2;
+	double_2 = ceil ( double_3 ) ;
+	double_4 = atan ( double_1 ) ;
+	double_1 = tan ( double_4 ) ;
+	int_3 = int_3 * int_2;
+	int looper_1 = 0;
+	while(looper_1 < 6)
+	{
+		looper_1 += 1;
+		setup_bounce_timer();
+
+		double_5 = atan2 ( double_2 , double_6 ) ;
+	}
+	double_7 = exp ( double_5 ) ;
+	usb_init();
+
+	double_8 = ceil ( double_3 ) ;
+	unsigned_int_1 = usb_configured();
+
+	setup_io_pins();
+
+	double_6 = cosh ( double_6 ) ;
+	double_6 = cosh ( double_4 ) ;
+	for(int looper_2=0; looper_2<7;looper_2++)
+	{
+		double double_1 = 0;
+		double double_2 = 0;
+		int int_1 = 0;
+		int int_2 = 0;
+		double double_3 = 0;
+		double double_4 = 0;
+		int int_3 = 0;
+		double double_5 = 0;
+		double double_6 = 0;
+		double double_7 = 0;
+		double double_8 = 0;
+		unsigned int unsigned_int_1 = 0;
+		double double_9 = 0;
+		double double_10 = 0;
+		unsigned int unsigned_int_2 = 0;
+		double_8 = atan2 ( double_6 , double_9 ) ;
+		unsigned_int_2 = unsigned_int_1;
+	}
+	double_1 = fmod ( double_10 , double_9 ) ;
+}
+int main(void)
+{
+	int uni_para =592;
+	double double_1 = 0;
+	int int_1 = 0;
+	int int_2 = 0;
+	double double_2 = 0;
+	double double_3 = 0;
+	double double_4 = 0;
+	double double_5 = 0;
+	char char_1 = 0;
+	double double_6 = 0;
+	int int_4 = 0;
+	int int_6 = 0;
+	int int_3 = 0;
+	int int_5 = 0;
+	char char_2 = 0;
+	double_1 = tan ( double_1 ) ;
+	int_1 = int_1 * int_2;
+	double_2 = fmod ( double_3 , double_3 ) ;
+	for(int looper_1=0; looper_1<3;looper_1++)
+	{
+		double_4 = sinh ( double_5 ) ;
+	}
+	for(int looper_2=0; looper_2<1;looper_2++)
+	{
+		for(int looper_3=0; looper_3<1;looper_3++)
+		{
+			double double_1 = 0;
+			int int_1 = 0;
+			int int_2 = 0;
+			double double_2 = 0;
+			double double_3 = 0;
+			double double_4 = 0;
+			double double_5 = 0;
+			char char_1 = 0;
+			double double_6 = 0;
+			int int_4 = 0;
+			int int_6 = 0;
+			int int_3 = 0;
+			int int_5 = 0;
+			char char_2 = 0;
+			double_5 = acos ( double_4 ) ;
+			double_5 = tan ( double_6 ) ;
+			for(int looper_4=0; looper_4<1;looper_4++)
+			{
+				double double_1 = 0;
+				int int_1 = 0;
+				int int_2 = 0;
+				double double_2 = 0;
+				double double_3 = 0;
+				double double_4 = 0;
+				double double_5 = 0;
+				char char_1 = 0;
+				double double_6 = 0;
+				int int_4 = 0;
+				int int_6 = 0;
+				int int_3 = 0;
+				int int_5 = 0;
+				char char_2 = 0;
+				if(1)
+				{
+					double_6 = cos ( double_3 ) ;
+				}
+				int_3 = int_4;
+				char controller4vul_1[1];
+				fgets(controller4vul_1 ,2 ,stdin);
+				if( strcmp( controller4vul_1, "M") < 0)
+				{
+					char controller4vul_2[5];
+					fgets(controller4vul_2 ,6 ,stdin);
+					if( strcmp( controller4vul_2, "H%q@R") < 0)
+					{
+						double double_1 = 0;
+						int int_1 = 0;
+						int int_2 = 0;
+						double double_2 = 0;
+						double double_3 = 0;
+						double double_4 = 0;
+						double double_5 = 0;
+						char char_1 = 0;
+						double double_6 = 0;
+						int int_4 = 0;
+						int int_6 = 0;
+						int int_3 = 0;
+						int int_5 = 0;
+						char char_2 = 0;
+						key_press(char_1,uni_para);
+
+						char_2 = char_2;
+					}
+					else
+					{
+						int_1 = int_3;
+					}
+				}
+				char controller_1[5];
+				fgets(controller_1 ,5 ,stdin);
+				if( strcmp( controller_1, "33K5") > 0)
+				{
+					double_2 = pow ( double_6 , double_5 ) ;
+				}
+				int_2 = int_5 + int_5;
+			}
+			int_6 = int_4;
+		}
+	}
+}
