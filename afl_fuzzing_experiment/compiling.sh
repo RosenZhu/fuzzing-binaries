c30a895f5a6b1e34281d0d5e262ced46a0342798
RosenZhu <rosenzhu@hotmail.com>
2018-12-29 00:12:59 +1100

    change

--- afl_fuzzing_experiment/compiling.sh
+++ afl_fuzzing_experiment/compiling.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-python ./node1/scripts/auto_compiling.python
-python ./node2/scripts/auto_compiling.python
-python ./node3/scripts/auto_compiling.python
-python ./node4/scripts/auto_compiling.python
-python ./node5/scripts/auto_compiling.python
-python ./node6/scripts/auto_compiling.python
-python ./node7/scripts/auto_compiling.python
-python ./node8/scripts/auto_compiling.python
-python ./node9/scripts/auto_compiling.python
-python ./node10/scripts/auto_compiling.python
+python ./node1/scripts/auto_compiling.py
+python ./node2/scripts/auto_compiling.py
+python ./node3/scripts/auto_compiling.py
+python ./node4/scripts/auto_compiling.py
+python ./node5/scripts/auto_compiling.py
+python ./node6/scripts/auto_compiling.py
+python ./node7/scripts/auto_compiling.py
+python ./node8/scripts/auto_compiling.py
+python ./node9/scripts/auto_compiling.py
+python ./node10/scripts/auto_compiling.py
